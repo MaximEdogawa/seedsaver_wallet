249d639f75401f51023ae4dd5fed7603c2814440
MaximEdogawa <leo@librem.one>
2023-03-11 23:42:49 +0100

    added result save to file, display of content and share function

--- lib/widgets/scan_result_widget.dart
+++ lib/widgets/scan_result_widget.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:share/share.dart';
 import 'package:flutter_zxing/flutter_zxing.dart';
+import 'package:path_provider/path_provider.dart';
 
-class ScanResultWidget extends StatelessWidget {
+class ScanResultWidget extends StatefulWidget {
   const ScanResultWidget({
     Key? key,
     this.result,
@@ -12,35 +15,118 @@ class ScanResultWidget extends StatelessWidget {
   final Function()? onScanAgain;
 
   @override
+  _ScanResultWidgetState createState() => _ScanResultWidgetState();
+}
+
+class _ScanResultWidgetState extends State<ScanResultWidget> {
+  late Future<String> _filePathFuture;
+  late String _fileContent;
+
+  @override
+  void initState() {
+    super.initState();
+    _filePathFuture = _initFilePath();
+    _writeToFile(widget.result?.text ?? '');
+    _fileContent = '';
+  }
+
+  @override
+  void didUpdateWidget(covariant ScanResultWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.result?.text != widget.result?.text) {
+      _writeToFile(widget.result?.text ?? '');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              result?.format?.name ?? '',
-              style: Theme.of(context).textTheme.headlineSmall,
-            ),
-            const SizedBox(height: 20),
-            Text(
-              result?.text ?? '',
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            const SizedBox(height: 20),
-            Text(
-              'Inverted: ${result?.isInverted}\t\tMirrored: ${result?.isMirrored}',
-              style: Theme.of(context).textTheme.bodyMedium,
-            ),
-            const SizedBox(height: 40),
-            ElevatedButton(
-              onPressed: onScanAgain,
-              child: const Text('Scan Again'),
-            ),
-          ],
+    return SingleChildScrollView(
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'File saved at:',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              const SizedBox(height: 20),
+              FutureBuilder<String>(
+                future: _filePathFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return GestureDetector(
+                      onTap: () async {
+                        final filePath = snapshot.data!;
+                        final file = File(filePath);
+                        final content = await file.readAsString();
+                        setState(() {
+                          _fileContent = content;
+                        });
+                      },
+                      child: Text(
+                        snapshot.data!,
+                        style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                              color: Theme.of(context).primaryColor,
+                              decoration: TextDecoration.underline,
+                            ),
+                      ),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    return CircularProgressIndicator();
+                  }
+                },
+              ),
+              const SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: widget.onScanAgain,
+                child: const Text('Scan Again'),
+              ),
+              const SizedBox(height: 20),
+              if (_fileContent.isNotEmpty) ...[
+                Container(
+                  height: 200,
+                  child: SingleChildScrollView(
+                    child: Text(_fileContent),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () async {
+                    final directory = await getApplicationDocumentsDirectory();
+                    final filePath = '${directory.path}/result.txt';
+                    final file = File(filePath);
+                    final content = await file.readAsString();
+                    final RenderBox renderBox =
+                        context.findRenderObject() as RenderBox;
+                    await Share.share(content,
+                        subject: 'File content',
+                        sharePositionOrigin:
+                            renderBox.localToGlobal(Offset.zero) &
+                                renderBox.size);
+                  },
+                  child: const Text('Share'),
+                ),
+              ],
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<String> _initFilePath() async {
+    final directory = await getApplicationDocumentsDirectory();
+    const fileName = 'result.txt';
+    final filePath = '${directory.path}/$fileName';
+    return filePath;
+  }
+
+  void _writeToFile(String text) async {
+    final file = File(await _filePathFuture);
+    await file.writeAsString(text);
   }
 }
